15e178e87cc8975ac517297d6002a825b2117924
fatmabadri <fbadri@my.bcit.ca>
2021-03-24 21:35:20 -0700

    update index.js within confectionery folder

--- mock-srv/routes/confectionery/index.js
+++ mock-srv/routes/confectionery/index.js
@@ -1,9 +1,22 @@
 'use strict'
+const fp = require('fastify-plugin')
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return [
-      {id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Delicious overpriced chocolate.'}
-    ]
-  })
+const catToPrefix = {
+  electronics: 'A',
+  confectionery: 'B'
 }
+
+const calculateID = (idPrefix, data) => {
+  const sorted = [...(new Set(data.map(({id}) => id)))]
+  const next = Number(sorted.pop().slice(1)) + 1
+  return `${idPrefix}${next}`
+}
+
+module.exports = fp(async function (fastify, opts) {
+  fastify.decorateRequest('mockDataInsert', function insert (category, data) {
+    const request = this
+    const idPrefix = catToPrefix[category]
+    const id = calculateID(idPrefix, data)
+    data.push({id, ...request.body})
+  })
+})
